4252c95a43f68bf964e458a7403f9f8e83a81ea6
Lawrence Lagerlof <lawrence@ead.fiocruz.br>
2017-06-26 11:46:19 -0300

    Class created.

---
+++ MyCsvPHP.php
@@ -0,0 +1,30 @@
+<?php
+
+class MyCsvPHP {
+
+	const OVERWRITE = 'w';
+	const APPEND = 'a';
+
+	public static function tableArrayToCsv($tableArray, $filePath, $fileWriteMode = self::OVERWRITE) {
+		$filePointer = fopen($filePath, $fileWriteMode) or die("Can't create or open file: '$filePath'");
+
+		if (($fileWriteMode == self::OVERWRITE) || (filesize($filePath) == 0)) {
+			fputcsv($filePointer, array_keys($tableArray[0]));
+		}
+
+		foreach($tableArray as $record) {
+			fputcsv($filePointer, $record);
+		}
+
+		fclose($filePointer) or die("Can't close the file: '$filePath'");
+	}
+}
+
+/*
+// Examples:
+// MyCsvPHP::tableArrayToCsv($tableArray, 'file.csv'); // Default is overwrite existing file.
+// MyCsvPHP::tableArrayToCsv($tableArray, 'file.csv', MyCsvPHP::OVERWRITE);
+// MyCsvPHP::tableArrayToCsv($tableArray, 'file.csv', MyCsvPHP::APPEND);
+*/
+
+?>

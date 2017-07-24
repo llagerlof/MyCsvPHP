b84cc7b3d10d2d22231a203f0fb51bd441c9d2b3
Lawrence Lagerlof <lawrence@ead.fiocruz.br>
2017-07-24 14:04:34 -0300

    Verify if array passed as parameter is empty.

--- MyCsvPHP.php
+++ MyCsvPHP.php
@@ -7,13 +7,16 @@ class MyCsvPHP {
 
 	public static function tableArrayToCsv($tableArray, $filePath, $fileWriteMode = self::OVERWRITE) {
 		$filePointer = fopen($filePath, $fileWriteMode) or die("Can't create or open file: '$filePath'");
+		$not_empty_array = (is_array($tableArray) && !empty($tableArray));
 
-		if (($fileWriteMode == self::OVERWRITE) || (filesize($filePath) == 0)) {
-			fputcsv($filePointer, array_keys($tableArray[0]));
-		}
+		if ($not_empty_array) {
+			if (($fileWriteMode == self::OVERWRITE) || (filesize($filePath) == 0)) {
+				fputcsv($filePointer, array_keys($tableArray[0]));
+			}
 
-		foreach($tableArray as $record) {
-			fputcsv($filePointer, $record);
+			foreach($tableArray as $record) {
+				fputcsv($filePointer, $record);
+			}
 		}
 
 		fclose($filePointer) or die("Can't close the file: '$filePath'");

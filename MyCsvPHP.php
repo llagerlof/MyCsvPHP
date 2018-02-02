ac62f1194e4822a4b013cfee841eb081a6f3ce6c
Lawrence Lagerlof <lawrence@ead.fiocruz.br>
2018-02-02 09:19:38 -0200

    Created a configuration file, MyCsvPHP.conf, with one option: enabled=true or false

--- MyCsvPHP.php
+++ MyCsvPHP.php
@@ -6,20 +6,36 @@ class MyCsvPHP {
 	const APPEND = 'a';
 
 	public static function tableArrayToCsv($tableArray, $filePath, $fileWriteMode = self::OVERWRITE) {
-		$filePointer = fopen($filePath, $fileWriteMode) or die("Can't create or open file: '$filePath'");
-		$not_empty_array = (is_array($tableArray) && !empty($tableArray));
+		if (self::enabled()) {
+			$filePointer = fopen($filePath, $fileWriteMode) or die("Can't create or open file: '$filePath'");
+			$not_empty_array = (is_array($tableArray) && !empty($tableArray));
 
-		if ($not_empty_array) {
-			if (($fileWriteMode == self::OVERWRITE) || (filesize($filePath) == 0)) {
-				fputcsv($filePointer, array_keys($tableArray[0]));
+			if ($not_empty_array) {
+				if (($fileWriteMode == self::OVERWRITE) || (filesize($filePath) == 0)) {
+					fputcsv($filePointer, array_keys($tableArray[0]));
+				}
+
+				foreach($tableArray as $record) {
+					fputcsv($filePointer, $record);
+				}
 			}
 
-			foreach($tableArray as $record) {
-				fputcsv($filePointer, $record);
-			}
+			fclose($filePointer) or die("Can't close the file: '$filePath'");
 		}
+	}
+	
+	private static function enabled() {
+		if (file_exists(dirname(__FILE__) . '/MyCsvPHP.conf')) {
+			$contents = file(dirname(__FILE__) . '/MyCsvPHP.conf');
+			foreach ($contents as $line) {
+				$config_value = explode('=', $line);
+				if (($config_value[0] == "enabled") && (trim($config_value[1]) == "false")) {
+					return false;
+				}
+			}
 
-		fclose($filePointer) or die("Can't close the file: '$filePath'");
+		}
+		return true;
 	}
 }
 
